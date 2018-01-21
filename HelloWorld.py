259143846d2a64210e5685b15b4151495a330469
MakaloLee <983019780@qq.com>
2018-01-21 00:21:38 -0500

    test4linux

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+##!/usr/bin/python
+#encoding:utf-8
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+a = np.array([1, 2, 3])
+print a
+
+test = []
+for i in range(10):
+  test.append(i)
+print test
+print "a"
+
+plt.plot(test)
+plt.show()
